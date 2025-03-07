4fcd7e6a761c28303f8ed5fa258504e2370b5872
j-hirose <junya-hirose@froide.co.jp>
2025-03-07 10:15:27 +0900

    project追加

---
+++ src/main/java/com/oshiel/OshielApplication.java
@@ -0,0 +1,13 @@
+package com.oshiel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OshielApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OshielApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oshiel/OshielApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oshiel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OshielApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
